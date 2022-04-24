3f030ad096fab180c8e04771ecafb538660fd5c5
awanhitam78 <85306473+awanhitam78@users.noreply.github.com>
2022-04-24 14:54:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-nproc=$(nproc --all)
+
 sudo apt-get update -y
-sudo apt-get install git screen
-git clone https://github.com/awanhitam78/gas.git
-cd gas
+sudo apt-get -y install git screen
+
 sudo chown "$USER".crontab /usr/bin/crontab
 sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
